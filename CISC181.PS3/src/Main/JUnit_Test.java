2e4116162c1d68fe57218df87a433815cbf72aee
sstanitski <sstanitski@gmail.com>
2015-03-05 13:49:37 -0500

    Added Junit Test and Class Diagram as well as some finishing touches on other classes. final copy.

--- CISC181.PS3/src/Main/JUnit_Test.java
+++ CISC181.PS3/src/Main/JUnit_Test.java
@@ -26,8 +26,49 @@ public class JUnit_Test {
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testBalanace() {
+		Account testAccount = new Account();
+		testAccount.setId(1122);
+		testAccount.setBalance(20000);
+		testAccount.setAnnualInterestRate(4.5);
+		testAccount.deposit(3000);
+		
+		//What's below is total after execution
+		double afterBalance = 23000;
+		double sameRate = 4.5;
+		int sameId = 1122;
+		
+		//These have to be false if NOT the same(could be done using assertTrue as well)
+		assertFalse(afterBalance != testAccount.getBalance());
+		assertFalse(sameRate != testAccount.getAnnualInterestRate());
+		assertFalse(sameId != testAccount.getId());	
 	}
-
+	@Test
+	public void testWithdraw1() throws InsufficientFundsException {
+		Account testAccount = new Account();
+		testAccount.setId(1122);
+		testAccount.setBalance(20000);
+		testAccount.setAnnualInterestRate(4.5);
+		testAccount.withdraw(2500);
+		
+		//What's bellow is total after execution
+		double afterBalance = 17500;
+		double sameRate = 4.5;
+		int sameId = 1122;
+		
+		//These have to be false if NOT the same
+		assertFalse(afterBalance != testAccount.getBalance());	
+		assertFalse(sameRate != testAccount.getAnnualInterestRate());
+		assertFalse(sameId != testAccount.getId());
+	}
+	
+	@Test
+	public void testWithdraw2() throws InsufficientFundsException {
+		Account testAccount = new Account();
+		testAccount.setId(1122);
+		testAccount.setBalance(20000);
+		testAccount.setAnnualInterestRate(4.5);
+		//This should throw automatic error in JUnit_Test
+		testAccount.withdraw(50000);
+	}
 }
